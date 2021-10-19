81c63e4a17514de9b3e349d9cd66f11c26dfdda0
David Kirton <kirtondavid53@gmail.com>
2021-10-18 21:50:41 -0400

    new commit

---
+++ app/src/main/java/com/example/assignment1/Product.java
@@ -0,0 +1,23 @@
+package com.example.assignment1;
+
+public class Product {
+
+    private String volume;
+    boolean icing;
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public void setIcing(boolean icing) {
+        this.icing = icing;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public boolean isIcing() {
+        return icing;
+    }
+}
--- app/src/main/java/com/example/assignment1/chocolatecake.java
+++ app/src/main/java/com/example/assignment1/chocolatecake.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class chocolatecake extends AppCompatActivity {
 
@@ -12,5 +17,24 @@ public class chocolatecake extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chocolatecake);
         Intent intent=getIntent();
+    }
+
+    public void onclickAddToCart(View view){
+        Product product = new Product();
+        CheckBox checkbox = (CheckBox) findViewById(R.id.icing);
+        boolean checked = checkbox.isChecked();
+        product.setIcing(checked);
+        //RadioButton radioButton = (RadioButton) findViewById(R.id)
+        RadioGroup radioGroup = (RadioGroup) findViewById((R.id.radio_group));
+        int id = radioGroup.getCheckedRadioButtonId();
+
+        if (id == -1){
+            TextView textView = (TextView) findViewById(R.id.textView3);
+            textView.setText("Please finish the form!");
+        }
+        else{
+            RadioButton radioButton = findViewById(id);
+        }
+
     }
 }
