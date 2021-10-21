787b89ad803b681118e1f2a9b783cd2c1eac559b
Admin <nicholasclifford05539@gmail.com>
2021-10-20 20:04:06 -0400

    all products lead to confirm order

--- app/src/main/java/com/example/assignment1/chocolatecake.java
+++ app/src/main/java/com/example/assignment1/chocolatecake.java
@@ -5,19 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class chocolatecake extends AppCompatActivity {
+
+    ArrayList<String> mylist=new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chocolatecake);
         Intent intent=getIntent();
+    }
+
+    public void onCheckboxClicked(View view)
+    {
+        CheckBox item =((CheckBox)view);
+        boolean v=item.isChecked();
+        String option=item.getText().toString();
+        if(v)
+        {
+            mylist.add(option);
+        }
+        else{
+            mylist.remove(option);
+        }
+
     }
 
     public void onclickAddToCart(View view){
@@ -30,16 +51,22 @@ public class chocolatecake extends AppCompatActivity {
         int id = radioGroup.getCheckedRadioButtonId();
 
         if (id == -1){
-            TextView textView = (TextView) findViewById(R.id.textView3);
-            textView.setText("Please finish the form!");
             Toast toast = Toast.makeText(getApplicationContext(), "Please select a type of cake", Toast.LENGTH_LONG);
             toast.show();
 
         }
         else{
-            RadioButton radioButton = findViewById(id);
-            String radio_text = radioButton.getText().toString();
-            product.setVolume(radio_text);
+            RadioButton type = findViewById(id);
+           // String radio_text = radioButton.getText().toString();
+           // product.setVolume(radio_text);
+
+            mylist.add(0,"Chocolate cake");
+            Button b = (Button) findViewById(radioGroup.getCheckedRadioButtonId());
+            mylist.add(b.getText().toString());
+            Intent intent = new Intent(this, Confirmorder.class);
+            intent.putStringArrayListExtra("list",mylist);
+            startActivity(intent);
+
         }
 
     }
--- app/src/main/java/com/example/assignment1/spongecake.java
+++ app/src/main/java/com/example/assignment1/spongecake.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class spongecake extends AppCompatActivity {
 
     RadioGroup type;
-    CheckBox icing;
+    ArrayList<String> mylist=new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,23 +26,38 @@ public class spongecake extends AppCompatActivity {
         Intent intent=getIntent();
     }
 
+    public void onCheckboxClicked(View view)
+    {
+        CheckBox item =((CheckBox)view);
+        boolean v=item.isChecked();
+        String option=item.getText().toString();
+        if(v)
+        {
+            mylist.add(option);
+        }
+        else{
+            mylist.remove(option);
+        }
 
+    }
 
     public void onclickAddToCart(View view)
     {
         type=(RadioGroup) findViewById(R.id.radio_group);
-        icing=(CheckBox) findViewById(R.id.icing);
 
-        if(type.getCheckedRadioButtonId()==-1)
+
+        if(type.getCheckedRadioButtonId()==-1 )
         {
             Toast toast = Toast.makeText(getApplicationContext(), "Please select a type of cake", Toast.LENGTH_LONG);
             toast.show();
         }
         else
         {
-
+            mylist.add(0,"Sponge cake");
+            Button b = (Button) findViewById(type.getCheckedRadioButtonId());
+            mylist.add(b.getText().toString());
             Intent intent = new Intent(this, Confirmorder.class);
-
+            intent.putStringArrayListExtra("list",mylist);
             startActivity(intent);
         }
 
